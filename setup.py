46ca04f174b8aa332206e5456beb9b3802e4c0f7
Martin Guerrero <slackmart@gmail.com>
2016-04-30 13:01:44 -0500

    Complete the setup for dist and add LICENSE.txt file

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ setup(
     version='v0.0.1',
     name='django-tastypie-sepomex',
     packages=find_packages(),
+    description='Little django application that exposes the sepomex database in a RESTful way.',
+    url='https://github.com/slackmart/tastypie-sepomex',
+    keywords=['sepomex', 'tastypie', 'django'],
     install_requires=[
         'django-tastypie'
     ],
